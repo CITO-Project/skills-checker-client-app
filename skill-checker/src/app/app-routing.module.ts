e0859cc8c3771f177391dc560b3567a2706b6aa4
Adrian Segade <correoestudiosadrian@gmail.com>
2019-11-05 14:02:39 +0000

    Prepared routing

--- skill-checker/src/app/app-routing.module.ts
+++ skill-checker/src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(
+    routes
+    // , { enableTracing: true } // <-- debugging purposes only
+    )],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
