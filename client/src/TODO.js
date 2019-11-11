6552de21a041e65214c02e43cbade7df318b3f00
Adrian Segade <correoestudiosadrian@gmail.com>
2019-11-11 12:11:52 +0000

    API moved
    API moved to a different repository. This is the final structure of the Client App

--- client/src/TODO.js
+++
@@ -1,1 +0,0 @@
-// TODO: Need to move header and bottom button as components. Also use flex containers to center. DO NOT USE MARGIN / PADDING
---
+++ src/TODO.js
@@ -0,0 +1,2 @@
+// TODO: Need to move header and bottom button as components. Also use flex containers to center. DO NOT USE MARGIN / PADDING
+// TODO: May it be a good approach to split the different 'product' entries in different databases?
