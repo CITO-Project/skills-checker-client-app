2dae5709a9bf481ed6399cb82450befe3fc9b3df
Adrian Segade <correoestudiosadrian@gmail.com>
2019-11-11 16:06:27 +0000

    Added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { OrientationScreenComponent } from './components/screens/orientation-screen/orientation-screen.component';
+import { InterestsScreenComponent } from './components/screens/interests-screen/interests-screen.component';
 
 
 const routes: Routes = [
   {
     path: '',
     component: OrientationScreenComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'interests',
+    component: InterestsScreenComponent,
     pathMatch: 'full'
   },
   {
