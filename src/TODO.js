d0e75ed29c9bc0f308163fbbd2680951fb83b903
Adrian Segade <correoestudiosadrian@gmail.com>
2019-11-12 12:07:00 +0000

    Interest screen created
    Created the V1.0 of the interests screen

--- src/TODO.js
+++ src/TODO.js
@@ -2,3 +2,4 @@
 // TODO: May it be a good approach to split the different 'product' entries in different databases?
 // TODO: Need to move node_modules to the parent directory. It is not working right now for some reason.
 // TODO: Add models to Interests
+// TODO: Remove console.log() in navigate-button
