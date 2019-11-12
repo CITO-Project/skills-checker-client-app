be8bb663ce46997e5eaee785d2c3ab227074382c
Adrian Segade <correoestudiosadrian@gmail.com>
2019-11-12 12:44:31 +0000

    Initial setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { OrientationScreenComponent } from './components/screens/orientation-screen/orientation-screen.component';
 import { InterestsScreenComponent } from './components/screens/interests-screen/interests-screen.component';
+import { HowToScreenComponent } from './components/screens/how-to-screen/how-to-screen.component';
 
 
 const routes: Routes = [
@@ -13,6 +14,11 @@ const routes: Routes = [
   {
     path: 'interests',
     component: InterestsScreenComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'how-to',
+    component: HowToScreenComponent,
     pathMatch: 'full'
   },
   {
---
+++ src/app/components/screens/how-to-screen/how-to-screen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-how-to-screen',
+  templateUrl: './how-to-screen.component.html',
+  styleUrls: ['./how-to-screen.component.scss']
+})
+export class HowToScreenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
