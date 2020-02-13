079e111d2a21a0b50fb99d69942ce9573b5812cb
Adrian Segade <adrian.segade@learnovatecentre.org>
2020-02-13 12:54:47 +0000

    Prepared client app for testing

--- src/app/components/screens/how-to-screen/how-to-screen.component.ts
+++ src/app/components/screens/how-to-screen/how-to-screen.component.ts
@@ -4,6 +4,7 @@ import { CommonService } from 'src/app/services/common.service';
 import { DataLogService } from 'src/app/services/data-log.service';
 import { ProgressTrackerService } from 'src/app/services/progress-tracker.service';
 import { Category } from 'src/app/models/category';
+import { Interest } from 'src/app/models/interest';
 
 @Component({
   selector: 'app-how-to-screen',
@@ -31,6 +32,23 @@ export class HowToScreenComponent implements OnInit {
   }
 
   retrieveInterest() {
+    // DELETE static interest and category. from interest-screen too
+    this.dataLogService.setInterest({
+      id: 3,
+      product: 1,
+      category: 1,
+      name: 'static_interest',
+      text: 'Static interest. Change afterwards'
+    });
+    this.dataLogService.setCategory({
+      id: 1,
+      product: 1,
+      name: 'personal',
+      text: 'Personal',
+      colour: 'green',
+      resource: 'person-laptop.svg'
+    });
+    // DELETE
     const interest = this.dataLogService.getInterest();
     if (!interest) {
       this.commonService.goTo('interests');
