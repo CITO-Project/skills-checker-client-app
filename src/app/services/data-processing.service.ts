972c525b6183201e724c5c839e36f99048a2d780
Adrian Segade <adrian.segade@learnovatecentre.org>
2020-02-18 13:01:24 +0000

    Rebuilt how data is processed
    Now data doesn't realy on the order and it looks for the right answers that has to be checked

--- src/app/services/data-processing.service.ts
+++ src/app/services/data-processing.service.ts
@@ -19,7 +19,7 @@ export class DataProcessingService {
     questionAnswers: Answer[][],
     answers: number[],
     challengingOrder: string[],
-    questionOrder: string[]) {
+    questionOrder: string[]): Result {
     const r: Result = {
       literacy: {
         level: 0,
@@ -34,39 +34,59 @@ export class DataProcessingService {
         priority: ''
       }
     };
-    let level = -1;
-    for (let i = 0; i < questions.length; i++) {
-      if (questions[i].pedagogical_type === 'challenging_skill') {
-        level++;
-        if (answers[i - 1] < questionAnswers[i - 1].length - 1 && answers[i] > 0) {
-          let answer = answers[i].toString(2);
-          while (answer.length < challengingOrder.length) {
-            answer = '0' + answer;
+    let level = 0;
+    let currentQuestions: Question[] = this.getItemsPerLevel(questions, questionOrder, level);
+    while (currentQuestions.length > 0) {
+      const currentAnswers: number[] = this.getItemsPerLevel(answers, questionOrder, level);
+      const currentQuestionAnswers: Answer[] = this.getItemsPerLevel(questionAnswers, questionOrder, level);
+      if (this.getItemByPedagogicalType(currentAnswers, questionOrder, 'task_question') < this.brushUpThreshold) {
+        let answer = this.getItemByPedagogicalType(currentAnswers, questionOrder, 'challenging_skill').toString(2);
+        while (answer.length < this.getItemByPedagogicalType(currentQuestionAnswers, questionOrder, 'challenging_skill').filter(
+          (questionAnswer: Answer) => {
+          if (questionAnswer.value >= 0) {
+            return true;
           }
-          for (let j = 0; j < answer.length; j++) {
-            if (answer.charAt(j) === '1' && r[challengingOrder.slice().reverse()[j]].level <= 0 ) {
-              const answerTaskQuestion = answers[questionOrder.length * level + 0];
-              let priority = 'none';
-              if (answerTaskQuestion < this.brushUpThreshold) {
-                priority = 'brush_up';
-                if (answerTaskQuestion < this.developThreshold) {
-                  priority = 'develop';
-                }
-              }
-              r[challengingOrder.slice().reverse()[j]] = {
-                level: level + 1,
-                priority
-              };
+        }).length) {
+          answer = '0' + answer;
+        }
+        answer = answer.split('').reverse().join('');
+        for (let i = 0; i < answer.length; i++) {
+          if (answer.charAt(i) === '1' && r[challengingOrder[i]].level <= 0) {
+            const answerTaskQuestion = this.getItemByPedagogicalType(currentAnswers, questionOrder, 'task_question');
+            let priority = 'brush_up';
+            if (answerTaskQuestion < this.developThreshold) {
+              priority = 'develop';
             }
+            r[challengingOrder[i]] = {
+              level: level + 1,
+              priority
+            };
           }
         }
       }
+      level++;
+      currentQuestions = this.getItemsPerLevel(questions, questionOrder, level);
     }
     return r;
   }
 
+  getItemsPerLevel(items: any[], questionOrder: string[], level: number): any[] {
+    const start = level * questionOrder.length;
+    return items.slice(start, start + questionOrder.length - 1);
+  }
+
+  getItemByPedagogicalType(items: any[], questionOrder: string[], pedagogicalType: string): any {
+    let r: any = null;
+    questionOrder.forEach( (question: string, index: number) => {
+      if (question === pedagogicalType) {
+        r = items[index];
+      }
+    });
+    return r;
+  }
+
   getResults(log: Log): Result {
-    return this.getChallengingSkills(log.questions, log.question_answers, log.answers, log.challenging_order, log.question_order) as Result;
+    return this.getChallengingSkills(log.questions, log.question_answers, log.answers, log.challenging_order, log.question_order);
   }
 
 }
