848f1bb7777738ab25d8c27b1931943695cb4287
Adrian Segade <adrian.segade@learnovatecentre.org>
2020-03-06 12:57:46 +0000

    Added question-order service and model

--- src/app/services/question-order.service.ts
+++ src/app/services/question-order.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { CommonService } from './common.service';
+
+import { QuestionOrder } from '../models/question-order';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class QuestionOrderService {
 
-  constructor() { }
+  constructor(private commonService: CommonService) { }
+
+  getQuestionOrder(): Observable<QuestionOrder[]> {
+    const url = '/questionorder';
+    return this.commonService.getAPICaller(url).pipe(map(
+      (data: QuestionOrder[]) => {
+        return data;
+      }
+    ));
+  }
 }
