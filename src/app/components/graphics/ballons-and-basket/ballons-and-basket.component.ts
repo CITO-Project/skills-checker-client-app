0617a03b0cbdafc0a5c788016c4b561bf2019f66
Adrian Segade <adrian.segade@learnovatecentre.org>
2020-03-09 17:36:56 +0000

    First iteration of balloons-and-basket with SVG

--- src/app/components/graphics/ballons-and-basket/ballons-and-basket.component.ts
+++ src/app/components/graphics/ballons-and-basket/ballons-and-basket.component.ts
@@ -1,4 +1,4 @@
-import { Component, AfterViewInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { GraphicsData } from 'src/app/models/graphics-data';
 import { CommonService } from 'src/app/services/common.service';
 
@@ -7,57 +7,30 @@ import { CommonService } from 'src/app/services/common.service';
   templateUrl: './ballons-and-basket.component.html',
   styleUrls: ['./ballons-and-basket.component.scss']
 })
-export class BallonsAndBasketComponent implements AfterViewInit {
+export class BallonsAndBasketComponent {
 
   @Input() data: GraphicsData[];
 
-  readonly svgNS = 'http://www.w3.org/2000/svg';
+  constructor(private commonService: CommonService) { }
 
-  private totalWeight = 0;
-
-  constructor(private commonService: CommonService) {
-  }
-
-  ngAfterViewInit() {
-    const el = document.getElementById('canvas');
-    const svg = this.createSVG(el);
-    el.append(svg);
-    this.createEllipse(svg, 50, 50, 20, 10, { stroke: '3', fill: 'red' });
-  }
-
-  createSVG(parent: HTMLElement, width: number = 100, height: number = 100, extras?: any): SVGSVGElement {
-    const el = document.createElementNS(this.svgNS, 'svg');
-    el.setAttribute('width', width + '');
-    el.setAttribute('height', height + '');
-    if (!!extras) {
-      Object.keys(extras).forEach( (key: string) => {
-        el.setAttribute(key, extras[key] + '');
-      });
+  /*
+Demo data:
+  [
+    {
+      text: 'Literacy'
+    },
+    {
+      text: 'Numeracy',
+      isRight: true
+    },
+    {
+      text: 'Digital skills'
+    },
+    {
+      text: 'Independence',
+      isRight: true
     }
-    parent.appendChild(el);
-    return el;
-  }
-
-  createEllipse(
-    parent: SVGSVGElement,
-    cx: number,
-    cy: number,
-    rx: number,
-    ry: number,
-    extras?: any
-    ): SVGEllipseElement {
-      const el = document.createElementNS(this.svgNS, 'ellipse');
-      el.setAttribute('cx', cx + '');
-      el.setAttribute('cy', cy + '');
-      el.setAttribute('rx', rx + '');
-      el.setAttribute('ry', ry + '');
-      if (!!extras) {
-        Object.keys(extras).forEach( (key: string) => {
-          el.setAttribute(key, extras[key] + '');
-        });
-      }
-      parent.append(el);
-      return el;
-  }
+  ]
+  */
 
 }
