d50493d6c62de733af342696f5814509f85f214d
Ian <ian.okeeffe@learnovatecentre.org>
2022-02-25 11:16:07 +0000

    Maltese translation

---
+++ src/environments/environment.prod.mt.ts
@@ -0,0 +1,46 @@
+export const environment = {
+  production: true,
+  domain: 'skillschecker.mt',
+  api: {
+    host: 'https://www.skillschecker.mt',
+    port: '',
+    path: '/api/',
+  },
+  readspeaker: {
+    enabled: false,
+    id: 12420,
+    lang: 'en_uk',
+    voice: 'Hugh',
+    baseurl: '//app-eu.readspeaker.com/cgi-bin/rsent'
+  },
+  analytics: {
+    google: {
+      id: 'UA-170127374-2'
+    }
+  },
+  download: {
+    level: true
+  },
+  product: 'nala-mt2',
+  locale: 'mt',
+  regions: [
+    'Online',
+    'Malta',
+    'Għawdex'
+  ],
+  regions_orig: [
+    'Online',
+    'Birkirkara',
+    'Blata l-Bajda Adult Learning Centre',
+    'Floriana',
+    'Għajnsielem (Gozo)',
+    'Ħal Far',
+    'Mosta',
+    'Paola',
+    'Qormi',
+    'Qormi Local Council',
+    'Tarxien Adult Learning Centre',
+    'Valletta',
+    'Żebbuġ'
+  ]
+};
